a596898ba277c0c78583cf65ec04fbc49d1396a6
George Leonidou <D881227@vmtech.local>
2023-02-11 17:32:13 +1100

    update region 2

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical 
 }
 
-resource "aws_instance" "web" { 
+resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
